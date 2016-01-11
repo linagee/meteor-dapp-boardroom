8323844b18fe35963daf5e7c99c2d12887c4b2e1
Nick Dodson <silentcicero@outlook.com>
2016-01-10 21:48:56 -0500

    Update FamilySystem.sol

--- contracts/v1/FamilySystem.sol
+++ contracts/v1/FamilySystem.sol
@@ -8,12 +8,14 @@ contract FamilySystem {
 	mapping(address => mapping(address => uint)) public toID;
 	mapping(address => mapping(uint => address)) public toMember;
 	mapping(address => mapping(address => bool)) public isMember;
+	mapping(address => uint) public numMembers;
 	
 	event MemberAdded(address _board, address _member, uint _memberID);
 	event MemberRemoved(address _board, address _member, uint _memberID);
 
 	function addMember(address _member, uint _type) public returns (uint memberID) {
 		memberID = members[msg.sender].length;
+		numMembers[msg.sender] += 1;
 		toID[msg.sender][_member] = memberID;
 		toMember[msg.sender][memberID] = _member;
 		isMember[msg.sender][_member] = true;
@@ -26,10 +28,15 @@ contract FamilySystem {
 		FamilyMember m = members[msg.sender][memberID];
 		m.position = 0;
 		m.member = address(0);
+		numMembers[msg.sender] -= 1;
 		delete toID[msg.sender][_member];
 		delete toMember[msg.sender][memberID];
 		isMember[msg.sender][_member] = false;
 		MemberRemoved(msg.sender, _member, memberID);
+	}
+	
+	function totalMembers(address _board) public constant returns (uint) {		
+		return members[_board].length;
 	}
 	
 	function memberPosition(address _board, uint _memberID) public constant returns (uint) {
